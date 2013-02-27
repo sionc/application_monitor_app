c3282dacc939d4a11230ceb17d7def9d3def4bbf
sionc <sion.chaudhuri@gmail.com>
2013-02-27 11:13:17 -0800

    [Setup] Track project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApplicationMonitorApp::Application.config.secret_token = 'a6e1c65d039d21eb8b1362f609a1cf17a8cc256381c29a5461bc547d2e43297f5f536dc469f7320db3a1614a17d7d7431d89c9a1e8f10aad83b52e32b8da4cd2'
